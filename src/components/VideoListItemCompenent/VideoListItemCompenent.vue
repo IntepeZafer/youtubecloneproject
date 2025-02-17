0dd68603b8a1111a798bfd753c9bf514e14c71dd
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-02-17 17:01:26 +0300

    Css Düzenlemeleri Yapıldı

--- src/components/VideoListItemCompenent/VideoListItemCompenent.vue
+++ src/components/VideoListItemCompenent/VideoListItemCompenent.vue
@@ -1,6 +1,9 @@
 <template>
-  <ul>
-    <li><img :src="imageUrl"></li>
+  <ul class="video-list-container">
+    <li class="video-list-item">
+      <img :src="imageUrl">
+      <p>{{videos.snippet.title}}</p>
+    </li>
   </ul>
 </template>
 
@@ -20,5 +23,21 @@ export default {
 </script>
 
 <style>
-
+  .video-list-container{
+    display: flex;
+    border: 1px solid black;
+    margin-top: 1rem;
+    padding: 1rem;
+    border-radius: 0.3rem;
+  }
+  .video-list-item{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+  }
+  .video-list-item img{
+    object-fit: cover;
+    border-radius: 0.3rem;
+  }
 </style>
