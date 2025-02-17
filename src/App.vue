9ec0fc615cbf1aaeb190619a28286047c5ca977b
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-02-17 13:49:29 +0300

    SearchBar Oluşturuldu Ve Css Düzenlemeleri Yapıldı

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="title">Youtube Clone Project</h1>
+    <SearchBarCompenent @search="searchTerm"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SearchBarCompenent from './components/SearchBarCompenent/SearchBarCompenent.vue';  
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBarCompenent
+  },
+  methods: {
+    searchTerm(searchTerm) {
+      console.log(searchTerm)
+    }  
   }
 }
 </script>
 
-<style></style>
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+    font-family: Arial, Helvetica, sans-serif;
+    scroll-behavior: smooth;
+  }
+  .container {
+    max-width: 75rem;
+    width: 100%;
+    background-color: rgb(255, 255, 255);
+    margin: 3.125rem auto;
+    font-size: 24px;
+    padding: 1rem;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+  }
+  .container h1{
+    text-align: center;  
+    font-weight: 100;
+    font-size: 1em;
+    letter-spacing: .125rem;
+    text-transform: capitalize;
+  }
+</style>
---
+++ src/components/SearchBarCompenent/SearchBarCompenent.vue
@@ -0,0 +1,56 @@
+<template>
+    <form @submit.prevent="handleSubmit" class="formContainer">
+        <input v-model="searchTerm" type="text" placeholder="Search">
+        <button type="submit">Search</button>
+    </form>
+</template>
+
+<script>
+export default {
+    emits : ['search'],
+    name : 'SearchBarCompenent',
+    data() {
+        return {
+            searchTerm : '',
+        }
+    },
+    methods: {
+        handleSubmit(){
+            this.$emit('search', this.searchTerm)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .formContainer{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+    .formContainer  input{
+        max-width: 90vw;
+        width: 100%;
+        border: 1px solid black;
+        outline: none;
+        padding: 0.5rem;
+        border-radius: 0.3rem;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+    }
+    .formContainer button{
+        max-width: 10vw;
+        width: 100%;
+        border: none;
+        outline: none;
+        padding: 0.5rem;
+        cursor: pointer;
+        background-color: black;
+        color: white;
+        border: 1px solid black;
+        border-radius: 0.3rem;
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+</style>
